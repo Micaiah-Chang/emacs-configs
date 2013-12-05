6ad80853e2dd9520e8bbc225f615543a36afc7b2
Micaiah-Chang <Micaiah@server.fake>
2013-12-04 16:29:11 -0800

    Modularize .emacs file and reconfigure .gitignore to work with change.
    Broke up all the disparate parts of .emacs to be loaded from .emacs.d/init.el
    instead. .gitignore used to also ignore all subdirectory files too. Corrected this.

---
+++ .emacs.d/packages.el
@@ -0,0 +1,73 @@
+;;------------------
+;;Package Management
+;;------------------
+
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil t)
+  (url-retrieve
+   "https://github.
+
+com/dimitri/el-get/raw/master/el-get-install.el"
+   (lambda (s)
+     (end-of-buffer)
+     (eval-print-last-sexp))))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+
+
+(push '(:name yasnippet
+              :website "https://github.com/capitaomorte/yasnippet.git"
+              :description "YASnippet is a template system for Emacs."
+              :type github
+              :pkgname "capitaomorte/yasnippet"
+              :features "yasnippet"
+              :compile "yasnippet.el")
+      el-get-sources)
+
+;;(el-get 'sync)
+
+;; Sync all my packages together so long as it has a recipe
+;; Note: The following has to be sync manually with git:
+;; auctex, flymake, powershell.el, pylint, request
+(setq my-packages (append
+				   '(el-get ein magit nxhtml
+							auto-complete geiser
+							multi-term package
+							paredit popup pymacs
+							python-mode quack
+							request undo-tree websocket
+							yasnippet zenburn-theme
+							smex)
+				   (mapcar 'el-get-source-name el-get-sources)))
+
+(el-get 'sync my-packages)
+
+
+;; (defun el-get-cleanup (packages)
+;;   "Remove packages not explicitly declared"
+;;   (let* ((packages-to-keep (el-get-dependencies (mapcar 'el-get-as-symbol packages)))
+;;          (packages-to-remove (set-difference (mapcar 'el-get-as-symbol
+;;                                                      (el-get-list-package-names-with-status
+;;                                                       "installed")) packages-to-keep)))
+;;     (mapc 'el-get-remove packages-to-remove)))
+
+;; (el-get-cleanup my-packages)
+
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(require 'package)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" .
+						  "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
---
+++ .emacs.d/python.el
@@ -0,0 +1,40 @@
+;;=======
+;; Python
+;;=======
+
+;; autoenables line number
+(add-hook 'python-mode-hook #'(lambda ()
+                                (linum-mode t)))
+
+(add-to-list 'load-path "~/.emacs.d/el-get/")
+(setq py-install-directory "./.emacs.d/el-get/")
+(require 'python-mode)
+;; Python Mode things
+
+(require 'ein)
+
+;; Django editing
+
+
+(autoload 'django-html-mumamo-mode "~/.emacs.d/el-get/nxhtml/autostart.el")
+(setq auto-mode-alist
+      (append '(("\\.html?$" . django-html-mumamo-mode)) auto-mode-alist))
+(setq mumamo-background-colors nil)
+(add-to-list 'auto-mode-alist '("\\.html$" . django-html-mumamo-mode))
+;; Workaround the annoying warnings:
+;; Warning (mumamo-per-buffer-local-vars):
+;; Already 'permanent-local t: buffer-file-name
+(when (and (>= emacs-major-version 24)
+		   (>= emacs-minor-version 2))
+  (eval-after-load "mumamo"
+	'(setq mumamo-per-buffer-local-vars
+		   (delq 'buffer-file-name mumamo-per-buffer-local-vars))))
+
+;;=================================================================
+;; (add-to-list 'load-path "~/.emacs.d/el-get/django-mode/")
+;; (require 'django-html-mode)
+;; (require 'django-mode)
+;; (yas/load-directory "~/.emacs.d/el-get/django-mode/snippets")
+;; (add-to-list 'auto-mode-alist '("\\.djhtml$" . django-html-mode))
+;;=================================================================
+;; Dunno if I need the above, will check on next startup
