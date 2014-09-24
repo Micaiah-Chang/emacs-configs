59e952478cb8198a626f0e856291229e2cdcd848
Micaiah-Chang <Micaiah@server.fake>
2014-09-24 00:03:12 -0700

    Remove term-config from windows.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,7 +4,8 @@
 (load-file "~/.emacs.d/elsp.el")
 (load-file "~/.emacs.d/keybindings.el")
 (load-file "~/.emacs.d/powershell-config.el")
-(load-file "~/.emacs.d/term-config.el")
+(unless (eq system-type 'windows-nt)
+  (load-file "~/.emacs.d/term-config.el"))
 (load-file "~/.emacs.d/python.el")
 
 (add-to-list 'load-path "~/.emacs.d/igor-mode-master")
