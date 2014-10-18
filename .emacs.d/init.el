ba5a47975b7006668da690fab9af6ad4f7c1eabb
Micaiah-Chang <Micaiah@server.fake>
2014-10-17 18:15:29 -0700

    Add Magit whitespace modification functions
    Ignore ^M carriage returns in git.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -36,6 +36,24 @@
 (global-set-key (kbd "\C-c g") 'magit-status)
 
 
+(defun magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w --ignore-space-at-eol" magit-diff-options)
+      (magit-dont-ignore-whitespace)
+    (magit-ignore-whitespace)))
+
+(defun magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w --ignore-space-at-eol")
+  (magit-refresh))
+
+(defun magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w --ignore-space-at-eol" magit-diff-options))
+  (magit-refresh))
+
+(define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
+
 ;;============
 ;; Global Modes
 ;;============
