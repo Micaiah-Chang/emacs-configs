9da6cca45c7b1475df01e009e46d702509c70f5e
Alan-Chang <alan.chang@varian.com>
2015-11-10 15:08:11 -0800

    Fix js3 specific config options

--- .emacs.d/lisp/js.el
+++ .emacs.d/lisp/js.el
@@ -1,11 +1,13 @@
 (add-to-list 'load-path "~/.emacs.d/el-get/jshint-mode")
+(add-to-list 'load-path "~/.emacs.d/el-get/js3-mode")
 
+(require 'js3-mode)
 
 (add-hook 'js3-mode-hook
 		  #'(lambda ()
-			  (js3-auto-indent-p t)   ; it's nice for commas to right themselves.
-			  (js3-enter-indents-newline t) ; don't need to push tab before typing
-			  (js3-indent-on-enter-key t)   ; fix indenting before moving on
+			  (setq js3-auto-indent-p t)   ; it's nice for commas to right themselves.
+			  (setq js3-enter-indents-newline t) ; don't need to push tab before typing
+			  (setq js3-indent-on-enter-key t)   ; fix indenting before moving on
 			  (define-key js-mode-map "{" 'paredit-open-curly)
 			  (define-key js-mode-map "}" 'paredit-close-curly-and-newline)
 			  (paredit-mode 1)))
