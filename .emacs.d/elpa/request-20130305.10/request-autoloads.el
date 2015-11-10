d10722d6de41fd0d0ee95b6dc66974cc04157e17
Alan-Chang <alan.chang@varian.com>
2015-11-10 15:07:49 -0800

    Delete unneeded request packages

--- .emacs.d/elpa/request-20130305.10/request-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; request-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("request-deferred.el" "request-pkg.el"
-;;;;;;  "request.el") (20858 21155 916000))
-
-;;;***
-
-(provide 'request-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; request-autoloads.el ends here
