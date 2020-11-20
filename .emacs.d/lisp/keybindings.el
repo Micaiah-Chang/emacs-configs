aa4dd5faacd4f8b5ad4b0cc37e3bde33044d10cd
Alan Chang <alan@simplelegal.com>
2020-11-20 11:39:01 -0800

    Modify insert-date to have better formatting

--- .emacs.d/lisp/keybindings.el
+++ .emacs.d/lisp/keybindings.el
@@ -33,12 +33,12 @@
       (setq arg (if (plusp arg) (1- arg) (1+ arg))))))
 
 (defun insert-date (prefix)
-  "Insert the current date. With prefix-argument, use ISO format. With
+  "Insert the current date as ISO. With prefix-argument, use American format. With
    two prefix arguments, write out the day and month name."
   (interactive "P")
   (let ((format (cond
-                 ((not prefix) "%m/%d/%Y")
-                 ((equal prefix '(4)) "%Y-%m-%d")
+                 ((not prefix) "%Y-%m-%d")
+                 ((equal prefix '(4)) "%m/%d/%Y")
                  ((equal prefix '(16)) "%A, %d. %B %Y")))
         (system-time-locale "de_DE"))
     (insert (format-time-string format))))
