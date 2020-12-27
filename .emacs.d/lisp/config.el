dca4ba0186905bf30ed2bd769518b4b10dd607be
Alan Chang <ahchang012@gmail.com>
2020-12-27 14:29:57 -0800

    Update config file
    Change (quote (...)) to '(..)
    Added various default installed packages
    Added multi-term-program as zsh by default
    Updated tls-checktrust call

--- .emacs.d/lisp/config.el
+++ .emacs.d/lisp/config.el
@@ -16,43 +16,40 @@
  '(company-idle-delay 0.7)
  '(cua-enable-cua-keys nil)
  '(cua-mode t nil (cua-base))
- '(custom-enabled-themes (quote (wombat)))
+ '(custom-enabled-themes '(wombat))
  '(custom-file "~/.emacs.d/lisp/config.el")
  '(custom-safe-themes
-   (quote
-    ("71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" default)))
+   '("71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" default))
  '(desktop-restore-eager 7)
- '(flycheck-check-syntax-automatically (quote (save idle-change mode- enabled)))
+ '(flycheck-check-syntax-automatically '(save idle-change mode- enabled))
  '(flycheck-idle-change-delay 4)
  '(geiser-racket-binary "C:\\Program files\\Racket\\Racket.exe")
  '(geiser-racket-gracket-binary "C:\\Program files\\Racket\\GRacket-text.exe")
  '(js3-indent-level 2)
- '(package-selected-packages (quote (pylint powershell let-alist flymake cl-generic)))
+ '(multi-term-program "/bin/zsh")
+ '(package-selected-packages
+   '(company-tabnine eterm-256color pylint powershell let-alist flymake cl-generic))
  '(py-indent-paren-spanned-multilines-p nil)
  '(safe-local-variable-values
-   (quote
-    ((python-shell-interpretor-args . "/home/alan/dev/eBill/manage.py shell")
-     (python-shell-interpretor . "python")
+   '((python-shell-interpretor . "python")
      (whitespace-style face tabs trailing lines-tail)
      (eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook"
-           (add-hook
-            (quote write-contents-functions)
-            (lambda nil
-              (delete-trailing-whitespace)
-              nil))
-           (require
-            (quote whitespace))
+           (add-hook 'write-contents-functions
+                     (lambda nil
+                       (delete-trailing-whitespace)
+                       nil))
+           (require 'whitespace)
            "Sometimes the mode needs to be toggled off and on."
            (whitespace-mode 0)
            (whitespace-mode 1))
      (whitespace-line-column . 80)
      (whitespace-style face trailing lines-tail)
-     (require-final-newline . t))))
+     (require-final-newline . t)))
  '(scroll-down-aggressively 0.4)
  '(scroll-up-aggressively 0.5)
  '(show-paren-mode t)
  '(standard-indent 2)
- '(tls-checktrust t))
+ '(tls-checktrust t t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
